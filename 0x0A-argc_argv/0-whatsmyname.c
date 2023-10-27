537ec9f22dea6ba28226ca975355cc360158f589
Miki-AA <michael.adissu@live.com>
2023-10-27 05:50:03 -0700

    k

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+		
+#include "main.h"
+		
+
+	       
+int main(int argc __attribute__((unused)), char *argv[])
+		
+{
+		
+	printf("%s\n", *argv);
+		
+
+		
+	return (0);
+		
+}
