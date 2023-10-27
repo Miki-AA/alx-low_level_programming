7534cc02a5fe427acd5d66fcc95c296b5717f16f
Miki-AA <michael.adissu@live.com>
2023-10-27 05:59:14 -0700

    p

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include "main.h"
-/**
-		
- * _putchar - writes the character c to stdout
-		
- * @c:Ther character to print
-		
- *
-		
- * Return: On success 1.
-		
- * On error, -1 is returned and errno is set appropriately.
-		
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));		
-}
 
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
