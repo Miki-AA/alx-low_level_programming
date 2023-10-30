aa5848e729b95fd1fa545088a7a2c3390b7ac80e
Miki-AA <michael.adissu@live.com>
2023-10-29 23:10:51 -0700

    message

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,44 +10,44 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int i, j;
+	char *concat;
+	int i, j;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    i = j = 0;
+	i = j = 0;
 
-    while (s1[i] != '\0')
-        i++;
+	while (s1[i] != '\0')
+		i++;
 
-    while (s2[j] != '\0')
-        j++;
+	while (s2[j] != '\0')
+		j++;
 
-    concat = malloc(sizeof(char) * (i + j + 1));
+	concat = malloc(sizeof(char) * (i + j + 1));
 
-    if (concat == NULL)
-        return (NULL);
+	if (concat == NULL)
+		return (NULL);
 
-    i = j = 0;
+	i = j = 0;
 
-    while (s1[i] != '\0')
-    {
-        concat[i] = s1[i];
-        i++;
-    }
+	while (s1[i] != '\0')
+	{
+		concat[i] = s1[i];
+		i++;
+	}
 
-    while (s2[j] != '\0')
-    {
-        concat[i] = s2[j];
-        i++;
-        j++;
-    }
+	while (s2[j] != '\0')
+	{
+		concat[i] = s2[j];
+		i++;
+		j++;
+	}
 
-    concat[i] = '\0';
+	concat[i] = '\0';
 
-    return (concat);
+	return (concat);
 }
